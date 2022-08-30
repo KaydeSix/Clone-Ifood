92f7bf739e6b01d6ba3167bd42bc400adec3d171
KaydeSix <srsenhorial@gmail.com>
2022-08-30 16:30:48 -0300

    Modificado o JS do cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,40 +1,37 @@
-const cadastrar = document.getElementById('click')
-cadastrar.addEventListener("click", transicaoIda)
-
-function transicaoIda(){
+function transicaoIdaEmail(){
     let oldForm = document.getElementById('formulario')
-    oldForm.remove()
+    oldForm.classList.add("hide")
 
-    let newForm = document.createElement("form")
-    let voltar = document.createElement("button")
-    voltar.id = "volte";
-    let descVoltar = document.createElement("p")
-    let title = document.createElement("h2")
-    let input = document.createElement("input")
-    input.type = "text"
-    let botao = document.createElement("button")
-    let descricao = document.createElement("p")
-
-    let botaoV = document.createTextNode("<")
-    let text = document.createTextNode("Informe o seu e-mail para continuar")
-    let botaoP = document.createTextNode("Continuar")
-
-    let element = document.getElementById("new-form")
-    element.appendChild(newForm)
-    newForm.appendChild(voltar)
-    descVoltar.appendChild(botaoV)
-    newForm.appendChild(title)
-    newForm.appendChild(input)
-    newForm.appendChild(botao)
-    voltar.appendChild(descVoltar)
-    botao.appendChild(descricao)
-    title.appendChild(text)
-    descricao.appendChild(botaoP)
-    
-    newForm.classList.add("form-email")
-    voltar.classList.add("botao-tunado")
-    title.classList.add("titulo")
-    input.classList.add("input-email")
-    botao.classList.add("button-form")
+    let newForm = document.getElementById('new-form')
+    newForm.classList.add('show')
 }
+
+function voltarForm(event){
+    event.preventDefault();
+
+    let newForm = document.getElementById('new-form')
+    newForm.classList.remove("show")
+    let oldForm = document.getElementById('formulario')
+    oldForm.classList.remove('hide')
+}
+
+function transicaoIdaSenha(event){
+    event.preventDefault();
+
+    let formEmail = document.getElementById('new-form')
+    formEmail.classList.remove('show')
+    let newFormSenha = document.getElementById('new-form-senha')
+    newFormSenha.classList.add('show')
+}
+
+const cadastrar = document.getElementById('click')
+cadastrar.addEventListener("click", transicaoIdaEmail)
+
+const voltar = document.getElementById("volte")
+voltar.addEventListener('click', voltarForm)
+
+const pedirSenha = document.getElementById('email-proximo')
+pedirSenha.addEventListener('click', transicaoIdaSenha)
+
+
 
